168d08665e8091205b934c9a6c83910270e6d398
SATHIYA PRIYAN R <110240557+SathiyaPriyanR@users.noreply.github.com>
2023-03-22 12:25:57 +0530

    Add files via upload

--- Basics/Samples/Main.java
+++ Basics/Samples/Main.java
@@ -2,18 +2,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Length of the Rectangle is : ");
-        int length = obj.nextInt();
+        System.out.println("The age of person 1 : ");
+        int age1 = scanner.nextInt();
 
-        System.out.println("Breadth of the Rectangle is : ");
-        int breadth = obj.nextInt();
+        System.out.println("The age of person 2 : ");
+        int age2 = scanner.nextInt();
 
-        if (length == breadth) {
-            System.out.println("Its Square");
-        } else {
-            System.out.println("Not a Square");
-        }
+        System.out.println("The age of person 3 : ");
+        int age3 = scanner.nextInt();
+
+        int oldest = (age1 > age2) ? ((age1 > age3) ? age1 : age3) : ((age2 > age3) ? age2 : age3);
+        int youngest = (age1 < age2) ? ((age1 < age3) ? age1 : age3) : ((age2 < age3) ? age2 : age3);
+
+        System.out.println("The Oldest person is " + oldest + " years old");
+        System.out.println("The youngest person is " + youngest + " years old");
+
     }
 }
